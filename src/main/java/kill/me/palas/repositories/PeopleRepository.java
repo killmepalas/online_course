8a5d46a6c172b91870922c817a4578aa96c36caa
killmepalas <killmepalas@gmail.com>
2022-05-02 14:49:21 +0300

    Revert "Initial commit"
    This reverts commit 58458f35113a967196d4223268eba1b5c0307382.

--- src/main/java/kill/me/palas/repositories/PeopleRepository.java
+++
@@ -1,7 +0,0 @@
-package kill.me.palas.repositories;
-
-import kill.me.palas.models.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PeopleRepository extends JpaRepository<Person, Integer> {
-}
